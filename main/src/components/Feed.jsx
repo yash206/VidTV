5c9fca1a896c96da31ba51d8c88219911034cab9
Yash Agarwal <agarwalyash9120076911@gmail.com>
2023-03-18 01:26:07 +0530

    created feed and sidebar

--- main/src/components/Feed.jsx
+++ main/src/components/Feed.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material'
+import { Sidebar } from '.';
 
-const Feed = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+
+const Feed = () => (
+    <Stack sx={{ flexDirection: { sx: "column", md:"row"}}} >
+    <Box sx={{ height: { sx: "auto", md:"92vh"}, borderRight: '1px solid #3d3d3d', px: {sx:0,md:2}}}>
+      <Sidebar/>
+      <Typography className='copyright' variant="body2" sx={{mt:1.5, color:'#fff'}}>
+        Copyright 2023 Yash Agarwal
+      </Typography>
+    </Box>
+    <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+      <Typography variant = "h4" fontWeight="bold" mb={2} sx={{color:'white'}}>
+        New <span style={{color: '#F31503'}}>videos</span>
+      </Typography>
+
+      <Videos/> // create Videos.jsx component
+    </Box>
+    </Stack>
+)
 
 export default Feed
